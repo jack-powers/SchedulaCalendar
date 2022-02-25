26172333fcb7a7168351e6f75221f70ada72c6ce
jack-powers <j.powers@connect.qut.edu.au>
2022-02-26 09:14:41 +1000

    a

--- SourceFiles/popup.js
+++ SourceFiles/popup.js
@@ -1,24 +1,36 @@
 // popup.js
 document.addEventListener('DOMContentLoaded', function () {
-  var checkButton = document.getElementById('check');
+  var checkButton = document.getElementById('google');
   checkButton.addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(
         tabs[0].id,
-        { method: "changePage" }
-      )
-      // .then(response => {
-      //   console.log("complete");
-      // }).catch(onError)
-
-
-
-        // function () {//response
-        // if (response.method == "changePage") {
-        //   // alert(response.text);
-        //   var i = 2; 
-        // }
-    });
-
+        { method: "google" }
+      )});
   }, false);
+  var checkButton = document.getElementById('outlook');
+  checkButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { method: "outlook" }
+      )});
+  }, false);
+  var checkButton = document.getElementById('outlookOnline');
+  checkButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { method: "outlookOnline" }
+      )});
+  }, false);
+  var checkButton = document.getElementById('office');
+  checkButton.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { method: "office" }
+      )});
+  }, false);
+
 }, false);
